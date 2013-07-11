f7bf708f6a99b4c60a12395e3e26a9c66c69e70b
jssuzanne <jssuzanne@localhost>
2013-07-11 19:26:59 +0200

    Add Route

--- web_longpolling/longpolling.py
+++ web_longpolling/longpolling.py
@@ -2,38 +2,76 @@
 
 from openerp.tools import config
 from multiprocessing import Process
-from simplejson import dumps
+from simplejson import dumps, loads
+from werkzeug.wrappers import Request, Response
+from werkzeug.routing import Map, Rule
+from werkzeug.exceptions import HTTPException
 
 
 LONGPOOLTIMEOUT = 5
 
 
-class Json(object):
+class LongPolling(object):
 
-    def __call__(self, function):
-        def wrap(*args, **kwargs):
-            result = function(*args, **kwargs)
-            print result
-            return dumps(result)
-        return wrap
+    path_map = Map()
+    view_function = {}
 
+    def route(self, path='/', mode='json', mustbeauthenticate=True):
+        assert path not in (False, None), "Bad route path: " + str(path)
+        assert isinstance(path, str), "Path must be a string: " + str(path)
+        assert mode in ('json', 'http'), "Mode must be json or http: " + str(path)
+        assert isinstance(mustbeauthenticate, bool)
 
-@Json()
-def application(request, start_response):
-    from gevent import sleep
-    status = '200 OK'
-    headers = [
-        ('Content-Type', 'application/json'),
-    ]
-    start_response(status, headers)
-    r = 'titi'
-    sleep(5)
-    return r
+        #TODO make authenticate
+        def wrapper(function):
+            if path[0] == '/':
+                realpath = '/openerplongpolling' + path
+            else:
+                realpath = '/openerplongpolling' + '/' + path
+            endpoint = '%s:%s' % (function.__name__, realpath)
+            rule = Rule(realpath, endpoint=endpoint)
+            self.path_map.add(rule)
+            old_func = self.view_function.get(endpoint)
+            if old_func is not None and old_func != function:
+                raise AssertionError('View function mapping is overwriting an '
+                                     'existing endpoint function: %s' % endpoint)
+            self.view_function[endpoint] = {
+                'function': function,
+                'mode': mode,
+            }
+            return function
+        return wrapper
+
+    def application(self, environ, start_response):
+        request = Request(environ)
+        response = self.dispatch_request(request)
+        return response(environ, start_response)
+
+    def dispatch_request(self, request):
+        adapter = self.path_map.bind_to_environ(request.environ)
+        try:
+            endpoint, values = adapter.match()
+            values.update(loads(request.args.get('data')))
+            request.db = request.args.get('db')
+            request.uid = request.args.get('uid')
+            result = self.view_function[endpoint]['function'](
+                request, **values)
+            if self.view_function[endpoint]['mode'] == 'json':
+                result = dumps(result)
+                mimetype = 'application/json'
+            else:
+                mimetype = 'text/html'
+
+            return Response(result, mimetype=mimetype)
+        except HTTPException, e:
+            return e
+
+longpolling = LongPolling()
 
 
 def process_longpolling(host, port):
     from gevent import pywsgi
-    server = pywsgi.WSGIServer((host, port), application)
+    server = pywsgi.WSGIServer((host, port), longpolling.application)
     server.serve_forever()
 
 
@@ -46,5 +84,13 @@ def start_server():
             p.deamon = True
             p.start()
 
+
+@longpolling.route('/<int>')
+def foo(request, **kwargs):
+    print kwargs
+    r = 'titi'
+    from gevent import sleep
+    sleep(5)
+    return r
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- web_longpolling/static/src/js/longpolling.js
+++ web_longpolling/static/src/js/longpolling.js
@@ -1,34 +1,50 @@
 openerp.web_longpolling = function(instance) {
     var ERROR_DELAY = 30000;
     instance.web.LongPolling = instance.web.Controller.extend({
-        init_longpolling: function(parent, service, success, error){
+        init_longpolling: function(parent, service, data, success, error){
             var self = this;
             this.longpolling_service = false;
             this.rpc('/web/longpolling/get_url', {}).then(function(url){
                 if (url) {
                     self.parent = parent;
-                    self.longpolling_service = '/openerplongpolling' + service;
+                    self.longpolling_service = '/openerplongpolling';
+                    self.longpolling_data = data || {};
+                    if (service.indexOf('/') === 0) 
+                        self.longpolling_service += service;
+                    else
+                        self.longpolling_service += '/' + service
                     self.longpolling_success = success || function(collection){};
-                    self.longpolling_error = error || function(collection){};
+                    //TODO call crashmanager
+                    self.longpolling_error = error || function(xhr, status, errorThrown){};
                     self.longpolling();
                 }
             });
         },
         longpolling: function(){
             var self = this;
+            console.log(this);
             $.ajax({
                 url: this.longpolling_service,
                 type: 'GET',
-                data: {},
+                data: {
+                    db: self.parent.session.db,
+                    uid: self.parent.session.uid,
+                    data: JSON.stringify(self.longpolling_data)
+                },
                 cache: false,
-                //success: this.longpolling_success,
-                //error: this.longpolling_error,
                 dataType: 'json',
-                //complete: longpolling, 
-                timeout: ERROR_DELAY
-            }).then(this.longpolling_success)
-              .fail(this.longpolling_error)
-              .done(function(unused, e) {self.longpolling();});
+                timeout: ERROR_DELAY,
+                complete: function(xhr, status, errorThrown){
+                    if (status === 'error' || !xhr.responseText) {
+                        self.longpolling_error(xhr, status, errorThrown);
+                        if (xhr.status !== 404) self.longpolling();
+                    } else {
+                        var data = xhr.responseText;
+                        self.longpolling_success(data);
+                        self.longpolling();
+                    }
+                }
+            })
         },
     });
     // just for test 
@@ -36,13 +52,12 @@ openerp.web_longpolling = function(instance) {
         show_common: function() {
             this._super();
             self.longpolling = new instance.web.LongPolling();
-            self.longpolling.init_longpolling(this, 'toto/tata/titi', function(yeah) {
+            self.longpolling.init_longpolling(
+                this, '/42', 
+                {toto: 'tata'},
+                function(yeah) {
                 console.log('success')
                 console.log(yeah)
-            }, function(jqXHR, textStatus) {
-                console.log('error')
-                console.debug(jqXHR)
-                console.debug(textStatus)
             });
         },
     });
