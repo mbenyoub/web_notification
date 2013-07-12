f42b7902d45e1f7900683de567d52050a9fc0edf
jssuzanne <jssuzanne@localhost>
2013-07-12 14:48:48 +0200

    Add crashmanager

--- web_longpolling/static/src/js/longpolling.js
+++ web_longpolling/static/src/js/longpolling.js
@@ -1,6 +1,10 @@
 openerp.web_longpolling = function(instance) {
     var ERROR_DELAY = 30000;
     instance.web.LongPolling = instance.web.Controller.extend({
+        init: function(){
+            this.longpolling_run = false;
+            this.crashmanager = new instance.web.CrashManager();
+        },
         start_longpolling: function(parent, service, data, success, error){
             var self = this;
             this.longpolling_service = false;
@@ -14,19 +18,30 @@ openerp.web_longpolling = function(instance) {
                     else
                         self.longpolling_service += '/' + service
                     self.longpolling_success = success || function(collection){};
-                    //TODO call crashmanager
-                    self.longpolling_error = error || function(xhr, status, errorThrown){};
+                    self.longpolling_error = error || function(xhr, status){
+                        if (xhr.status !== 502) {
+                            var error = {
+                                code: xhr.status,
+                                message: "XmlHttpRequestError ",
+                                data: {
+                                    type: "xhr" + status, 
+                                    debug: xhr.responseText, 
+                                    objects: [xhr] 
+                                }
+                            };
+                            self.crashmanager.rpc_error(error);
+                        }
+                    };
                     self.longpolling_run= true;
                     self.longpolling();
                 }
             });
         },
-        stop_longpolling= function(){
+        stop_longpolling: function(){
             this.longpolling_run = false;
         },
         longpolling: function(){
             var self = this;
-            console.log(this);
             $.ajax({
                 url: this.longpolling_service,
                 type: 'GET',
@@ -41,7 +56,7 @@ openerp.web_longpolling = function(instance) {
                 complete: function(xhr, status, errorThrown){
                     if (status === 'error' || !xhr.responseText) {
                         self.longpolling_error(xhr, status, errorThrown);
-                        if (xhr.status !== 404) self.longpolling_run = false;
+                        if (xhr.status === 404) self.longpolling_run = false;
                     } else {
                         var data = xhr.responseText;
                         self.longpolling_success(data);
