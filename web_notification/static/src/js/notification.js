36e2d6ada2671e3103dde3beeb458ae58cc4499f
jssuzanne <jssuzanne@localhost>
2014-07-10 12:13:12 +0200

    Update use bus_enhanced

--- web_notification/static/src/js/notification.js
+++ web_notification/static/src/js/notification.js
@@ -1,21 +1,10 @@
 (function() {
-    var web_notification = openerp.web_notification = {};
-
-    web_notification.Notification = openerp.Widget.extend({
-        init: function(parent, user_id) {
-            var self = this;
-            this._super(parent);
-            this.bus = openerp.bus.bus;
-            this.channel = 'notify_res_user_' + user_id;
-            this.bus.add_channel(this.channel)
-            this.bus.on("notification", this, this.on_notification);
-            this.bus.start_polling();
-        },
-        on_notification: function(notification) {
-            var self = this;
-            var channel = notification[0];
-            var message = notification[1];
-            if (channel == this.channel) {
+    openerp.web.WebClient.include({
+        declare_bus_channel: function() {
+            this._super();
+            var self = this,
+                channel = 'notify_res_user_' + this.session.uid;
+            this.on(channel, this, function(message) {
                 if (message.mode == 'warn'){
                     self.do_warn(message.subject,
                                  message.body,
@@ -27,13 +16,8 @@
                                        message.sticky);
                     }
                 }
-            }
-        },
-    });
-    openerp.web.WebClient.include({
-        show_application: function() {
-            this._super();
-            this.web_notification = new web_notification.Notification(this, this.session.uid);
+            });
+            this.add_bus_channel(channel);
         },
     });
 })();
